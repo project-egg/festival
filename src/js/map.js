00b1c0256ea689e96eecf5722d1e23ac248609ed
inhyeokoh <oredtiger@naver.com>
2024-11-21 20:17:10 +0900

    add highlighting selected

--- src/js/map.js
+++ src/js/map.js
@@ -47,6 +47,16 @@ function createMarkerAndOverlay(position, festival, index) {
   const overlayLink = content.querySelector('.overlay');
   overlayLink.addEventListener('click', (e) => {
     e.preventDefault(); // 기본 링크 동작 방지
+    // 모든 제목에서 선택 클래스 제거
+    const allTitles = document.querySelectorAll(".customoverlay .title");
+    allTitles.forEach((title) => {
+      title.classList.remove("selected-title");
+    });
+
+    // 현재 클릭된 제목에 선택 클래스 추가
+    const titleElement = overlayLink.querySelector('.title');
+    titleElement.classList.add("selected-title");
+
     openModal(festival); // 축제 데이터 전달
   });
 
@@ -56,24 +66,36 @@ function createMarkerAndOverlay(position, festival, index) {
   $festival_summary.classList.add("festival-summary");
   $festival_summary.innerHTML = festival.fstvlNm;
   $festival_summary.dataset.index = index; // 생성된 순서에 따른 인덱스 부여
-  $festival_summary.addEventListener("click", (e) => selectMarker(e));
+  $festival_summary.addEventListener("click", (e) => 
+    HighlightMarkerAndSummary(e.target.dataset.index));
   $festival_info.append($festival_summary);
 }
 
-function selectMarker(e) {  
+// TODO : 변수 정리 해주는게 좋을듯 
+function HighlightMarkerAndSummary(i) {  
+  // Highlight Marker 
   const allTitles = document.querySelectorAll(".customoverlay .title");
   
+  // 기존 선택 요소 제거
   allTitles.forEach((title) => {
     title.classList.remove("selected-title");
-  });
+  });  
 
-  // 선택된 마커의 제목에 선택 클래스 추가
-  const index = e.target.dataset.index; // dataset.index 사용
-  
-  const selectedTitle = allTitles[index]; // 인덱스를 사용하여 선택된 요소 찾기
+  const selectedTitle = allTitles[i]; // 인덱스를 사용하여 선택된 요소 찾기
   if (selectedTitle) {
     selectedTitle.classList.add("selected-title");
-  }
+  };
+  
+  // Highlight Summary
+  const allSummaries = Array.from($festival_info.children); // $festival_info의 자식 요소를 배열로 변환
+  allSummaries.forEach((info) => {
+    info.classList.remove("selected-summary");
+  });  
+
+  const selectedInfo = $festival_info.children[i]; // 인덱스를 사용하여 선택된 요소 찾기
+  if (selectedInfo) {
+    selectedInfo.classList.add("selected-summary");
+  };
 }
 
 function openModal(festival) {
