70b074b58a17fce4ad1b27027e5011d647fd95b7
inhyeokoh <oredtiger@naver.com>
2024-11-21 18:38:38 +0900

    add feature select-markers

--- src/js/map.js
+++ src/js/map.js
@@ -1,55 +1,94 @@
 import festivalDatas from "./festival.js";
 
-const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+const $festival_info = document.querySelector(".festival-info"); // 축제 정보 영역의 DOM 레퍼런스
+
 const options = {
-  //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(36, 127.6), //지도의 중심좌표 위도(latitude), 경도(longitude)순
-  level: 13, //지도의 레벨(확대, 축소 정도)
+  center: new kakao.maps.LatLng(36, 127.6), // 지도의 중심좌표
+  level: 13, // 지도의 레벨
 };
 
-const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+const geocoder = new kakao.maps.services.Geocoder(); // 주소-좌표 변환 객체 생성
 
-// TODO : 마커 사이즈에 따른 ImageOption offset 변동하도록 수정
-festivalDatas.forEach((festival) => {
-  var imageSrc = "../../assets/images/festival.png", // 마커이미지의 주소입니다
-    imageSize = new kakao.maps.Size(25, 25), // 마커이미지의 크기입니다
-    imageOption = { offset: new kakao.maps.Point(12.5, 12.5) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+// 마커와 커스텀 오버레이를 생성하는 함수
+function createMarkerAndOverlay(position, title, index) {
+  const markerImage = new kakao.maps.MarkerImage(
+    "../../assets/images/festival.png",
+    new kakao.maps.Size(25, 25),
+    { offset: new kakao.maps.Point(12.5, 12.5) }
+  );
 
-  // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-  var markerImage = new kakao.maps.MarkerImage(
-      imageSrc,
-      imageSize,
-      imageOption
-    ),
-    markerPosition = new kakao.maps.LatLng(
-      festival.latitude,
-      festival.longitude
-    ); // 마커가 표시될 위치입니다
-
-  // 마커를 생성합니다
-  var marker = new kakao.maps.Marker({
-    position: markerPosition,
-    image: markerImage, // 마커이미지 설정
+  const marker = new kakao.maps.Marker({
+    position: position,
+    image: markerImage,
   });
 
-  // 마커가 지도 위에 표시되도록 설정합니다
-  marker.setMap(map);
+  marker.setMap(map); // 마커를 지도 위에 표시
 
-  // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-  var content = `<div class="customoverlay">
-            <a href="https://map.kakao.com/link/map/11394059" target="_blank">
-              <span class="title">${festival.fstvlNm}</span>
-            </a>
-          </div>`;
+  const content = `<div class="customoverlay">
+          <a href="https://map.kakao.com/link/map/11394059" target="_blank">
+            <span class="title">${title}</span>
+          </a>
+        </div>`;
 
-  // 커스텀 오버레이가 표시될 위치입니다
-  var position = new kakao.maps.LatLng(festival.latitude, festival.longitude);
-
-  // 커스텀 오버레이를 생성합니다
-  var customOverlay = new kakao.maps.CustomOverlay({
+  const customOverlay = new kakao.maps.CustomOverlay({
     map: map,
     position: position,
     content: content,
     yAnchor: 0,
   });
+
+  // 축제 정보 동적 생성
+  const $festival_summary = document.createElement("p");
+  $festival_summary.classList.add("festival-summary");
+  $festival_summary.innerHTML = title;
+  $festival_summary.dataset.index = index; // 생성된 순서에 따른 인덱스 부여
+  $festival_summary.addEventListener("click", (e) => selectMarker(e));
+  $festival_info.append($festival_summary);
+}
+
+// 축제 데이터를 기반으로 마커와 오버레이 생성
+let currentIndex = 0; // 현재 인덱스를 추적하는 변수
+
+festivalDatas.forEach((festival) => {
+  const latitude = parseFloat(festival.latitude);
+  const longitude = parseFloat(festival.longitude);
+
+  // 위도와 경도 유효성 검사
+  if (
+    isNaN(latitude) ||
+    isNaN(longitude) ||
+    latitude < -90 ||
+    latitude > 90 ||
+    longitude < -180 ||
+    longitude > 180
+  ) {
+    // 주소로 좌표를 검색
+    geocoder.addressSearch(festival.rdnmadr, function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        createMarkerAndOverlay(coords, festival.fstvlNm, currentIndex++); // 마커와 오버레이 생성
+      }
+    });
+  } else {
+    const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+    createMarkerAndOverlay(markerPosition, festival.fstvlNm, currentIndex++); // 마커와 오버레이 생성
+  }
 });
+
+function selectMarker(e) {  
+  const allTitles = document.querySelectorAll(".customoverlay .title");
+  
+  allTitles.forEach((title) => {
+    title.classList.remove("selected-title");
+  });
+
+  // 선택된 마커의 제목에 선택 클래스 추가
+  const index = e.target.dataset.index; // dataset.index 사용
+  
+  const selectedTitle = allTitles[index]; // 인덱스를 사용하여 선택된 요소 찾기
+  if (selectedTitle) {
+    selectedTitle.classList.add("selected-title");
+  }
+}
