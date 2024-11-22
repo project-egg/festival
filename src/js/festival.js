2f70f2d987857ea532997a24fea49dffe4af7aee
samchi <seyoung0314@gmail.com>
2024-11-22 15:15:36 +0900

    데이터 id 값 추가

--- src/js/festival.js
+++ src/js/festival.js
@@ -24,8 +24,24 @@ async function fetchFestivalData(apiUrl) {
     
     const jsonData = xmlToJson(XmlNode);
     const festivalRes = jsonData.response.body.items.item;
+    let index = 0;
     const filteredByTodayData = festivalRes.filter((data)=>{
       return data.fstvlEndDate >= today;
+    }).map(data=>{
+      return {
+        auspcInsttNm: data.auspcInsttNm,
+        fstvlCo: data.fstvlCo,
+        fstvlEndDate: data.fstvlEndDate,
+        fstvlNm: data.fstvlNm,
+        fstvlStartDate: data.fstvlStartDate,
+        homepageUrl: data.homepageUrl,
+        lnmadr: data.lnmadr,
+        mnnstNm: data.mnnstNm,
+        opar: data.opar,
+        phoneNumber: data.phoneNumber,
+        rdnmadr: data.rdnmadr,
+        id: index++, // id 값에 인덱스를 1씩 증가
+      };
     })
 
     return filteredByTodayData;
